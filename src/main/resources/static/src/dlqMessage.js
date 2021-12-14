1699d72c3970650b33f65e577123b3aad5d4bd4a
zhangjidi2016 <zhangjidi@cmss.chinamobile.com>
2021-12-14 11:10:19 +0800

    [ISSUE #50]Modify the suffix of the interface name.

--- src/main/resources/static/src/dlqMessage.js
+++ src/main/resources/static/src/dlqMessage.js
@@ -182,7 +182,7 @@ module.controller('dlqMessageController', ['$scope', 'ngDialog', '$http', 'Notif
     };
 
     $scope.exportDlqMessage = function (msgId, consumerGroup) {
-        window.location.href = "dlqMessage/exportDlqMessage.query?msgId=" + msgId + "&consumerGroup=" + consumerGroup;
+        window.location.href = "dlqMessage/exportDlqMessage.do?msgId=" + msgId + "&consumerGroup=" + consumerGroup;
     };
 
     $scope.selectedDlqMessage = [];
@@ -227,7 +227,7 @@ module.controller('dlqMessageController', ['$scope', 'ngDialog', '$http', 'Notif
         }
         $http({
             method: "POST",
-            url: "dlqMessage/batchExportDlqMessage.query",
+            url: "dlqMessage/batchExportDlqMessage.do",
             data: $scope.selectedDlqMessage,
             headers: {
                 'Content-type': 'application/json'
