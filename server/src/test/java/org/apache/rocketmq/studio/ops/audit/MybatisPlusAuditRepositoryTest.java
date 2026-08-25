fc01eff21eb35dfeb7ba3d58503e118b823393f6
liuhy <liuhongyu@apache.org>
2026-08-25 01:58:24 -0700

    test: align test method naming
    Signed-off-by: liuhy <liuhongyu@apache.org>

--- server/src/test/java/org/apache/rocketmq/studio/ops/audit/MybatisPlusAuditRepositoryTest.java
+++ server/src/test/java/org/apache/rocketmq/studio/ops/audit/MybatisPlusAuditRepositoryTest.java
@@ -51,7 +51,7 @@ class MybatisPlusAuditRepositoryTest {
     private MybatisPlusAuditRepository repository;
 
     @Test
-    void findPageUsesMapperPaginationAndPreservesAuditContext() {
+    void findPageUsesMapperPaginationAndPreservesAuditContextTest() {
         RmqOperationAudit entity = new RmqOperationAudit();
         entity.setId(42L);
         entity.setOperation("DELETE_TOPIC");
@@ -86,7 +86,7 @@ class MybatisPlusAuditRepositoryTest {
     }
 
     @Test
-    void findFilterOptionsPreservesPersistedValuesAndCachesTheResult() {
+    void findFilterOptionsPreservesPersistedValuesAndCachesTheResultTest() {
         when(auditMapper.selectMaps(any(Wrapper.class))).thenReturn(List.of(
                 Map.of("operation", "DELETE_TOPIC", "resource_type", "TOPIC",
                         "cluster_id", "prod-cn", "result", "SUCCESS"),
@@ -151,7 +151,7 @@ class MybatisPlusAuditRepositoryTest {
     }
 
     @Test
-    void saveInvalidatesCachedFilterOptions() {
+    void saveInvalidatesCachedFilterOptionsTest() {
         when(auditMapper.selectMaps(any(Wrapper.class))).thenReturn(List.of());
 
         repository.findFilterOptions();
