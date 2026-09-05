d13e7c1cd5a2b4b548181494508fb9d9db521e7c
Yue Wang <1939455790@qq.com>
2026-09-05 23:56:37 +0800

    test: cover envelope and nested validation in AlertRuleTransferDTO

--- server/src/test/java/org/apache/rocketmq/studio/ops/alert/AlertRuleTransferDTOTest.java
+++ server/src/test/java/org/apache/rocketmq/studio/ops/alert/AlertRuleTransferDTOTest.java
@@ -29,4 +29,32 @@ class AlertRuleTransferDTOTest {
                 .extracting(violation -> violation.getMessage())
                 .contains("rule must not be null");
     }
+
+    @Test
+    void rejectsMissingEnvelopeFieldsTest() {
+        AlertRuleTransferDTO transfer = new AlertRuleTransferDTO();
+
+        assertThat(validator.validate(transfer))
+                .extracting(violation -> violation.getMessage())
+                .contains("version is required", "domain is required", "rules must not be empty");
+    }
+
+    @Test
+    void acceptsACompleteTransferWithoutViolationsTest() {
+        AlertRuleRequestDTO rule = new AlertRuleRequestDTO();
+        rule.setName("Broker unavailable");
+        rule.setMetric("broker.up");
+        rule.setOperator("==");
+        rule.setThreshold(0);
+        rule.setDuration("1m");
+        rule.setChannels(java.util.List.of("email"));
+        rule.setEnabled(true);
+
+        AlertRuleTransferDTO transfer = new AlertRuleTransferDTO();
+        transfer.setVersion(AlertRuleTransferDTO.VERSION);
+        transfer.setDomain(AlertDomain.CLUSTER);
+        transfer.setRules(java.util.List.of(rule));
+
+        assertThat(validator.validate(transfer)).isEmpty();
+    }
 }
